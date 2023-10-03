7b225b4ada3645a75a8be46261dae72908fa53fd
Cyrus Lee <1201996@learn.vsb.bc.ca>
2023-10-02 23:32:25 -0700

    Picture Project

---
+++ PictureProject.java
@@ -0,0 +1,15 @@
+public class PictureProject {
+    public  static void main(String[] args){
+        System.out.println("    _________   ");
+        System.out.println("   /         \\");
+        System.out.println("   |  o   o  |");
+        System.out.println("   |    l    |");
+        System.out.println("   \\   ---   /");
+        System.out.println("    \\_______/");
+        System.out.println("Computer Programming 11 - VLN");
+        System.out.println("Cyrus Lee");
+        System.out.println("02/10/23");
+
+
+    }
+}
